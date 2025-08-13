7ca4f5b5f54a206a96da5535824f7237e60894e0
WongtonMein <190700700+WongtonMein@users.noreply.github.com>
2025-08-13 13:47:18 -0700

    Complete JS for base game

--- javascript.js
+++ javascript.js
@@ -4,46 +4,99 @@ console.log("Hello World")
 // 1 = "paper"
 // 2 = "scissors"
 
-// GET COMPUTER CHOICE FUNCTION
+// getComputerChoice function
 function getComputerChoice() {
 
-    // USE MATH.RANDOM TO DETERMINE
+    // use Math.random() to determine computerChoice
     let computerInt = Math.floor(Math.random() * 3)
 
-    // TEST WITH CONSOLE.LOG
-    console.log(computerInt)
+    // test with console.log()
+    // console.log(computerInt)
 
-    // RETURN COMPUTER CHOICE
+    // return computerChoice as number value
     return computerInt
 };
 
-// GET HUMAN CHOICE FUNCTION
+// getHumanChoice function
 function getHumanChoice() {
-    return null
-};
 
-    // PROMPT HUMAN FOR ROCK, PAPER, OR SCISSORS
-        // NOT CASE SENSITIVE
-    // TEST WITH CONSOLE.LOG
-    // RETURN HUMAN CHOICE
+    // prompt human for rock, paper, or scissors
+    let humanChoice = prompt("Rock, Paper, or Scissors?")
+
+    // prompt changed toLowerCase() to remove case sensitivity
+    humanChoice = humanChoice.toLowerCase()
+
+    // test with console.log()
+    // console.log(humanChoice)
+
+    // return humanChoice as number value
+    if (humanChoice == "rock")
+        return 0
+    else if (humanChoice == "paper")
+        return 1
+    else if (humanChoice == "scissors")
+        return 2
+};
     
-function playRound(humanChoice, computerChoice) {
-    return null
+// helper function to convert number value to word for playRound() console.log output
+function convertNumberToWord(choice) {
+    if (choice == 0)
+        return "Rock"
+    else if (choice == 1)
+        return "Paper"
+    else if (choice == 2)
+        return "Scissors"
+}
+
+// playRound function - parameters getComputerChoice, getHumanChoice
+function playRound(computerChoice, humanChoice) {
+
+    let humanChoiceWord = convertNumberToWord(humanChoice)
+    let computerChoiceWord = convertNumberToWord(computerChoice)
+
+    // console.log() results
+    if (humanChoice == computerChoice) {
+        console.log("Tie!");
+    } else if (humanChoice == computerChoice + 1 || humanChoice == computerChoice - 2) {
+        console.log("You win! " + humanChoiceWord + " beats " + computerChoiceWord)
+        humanScore++ // increment humanScore variable
+    } else {
+        console.log("You lose! " + computerChoiceWord + " beats " + humanChoiceWord)
+        computerScore++ // increment computerScore variable
+    }
 };
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-// CREATE HUMANSCORE VARIABLE
+// create humanScore variable
 let humanScore = 0;
 
-// CREATE COMPUTERSCORE VARIABLE
+// create computerScore variable
 let computerScore = 0;
 
-playRound(humanSelection, computerSelection)
+// playGame function
+function playGame() {
+    let i = 0
 
-// PLAYROUND FUNCTION
-    // GETCOMPUTERCHOICE FUNCTION
-    // GETHUMANCHOICE FUNCTION
-    // CONSOLE.LOG RESULTS
-    // INCREMENT ____SCORE VARIABLES
+    // sets number of rounds
+    let gameRounds = 5
+
+    // calls playRound()
+    while (i < gameRounds) {
+
+        // prompt for new humanSelection
+        const computerSelection = getComputerChoice()
+        const humanSelection = getHumanChoice()
+
+        // play round
+        playRound(humanSelection, computerSelection)
+
+        // console.log() current scores
+        console.log("Player score: " + humanScore)
+        console.log("Computer score: " + computerScore)
+
+        // increments game round counter
+        i++;
+    }
+};
+    
+// calls playGame()
+playGame()
