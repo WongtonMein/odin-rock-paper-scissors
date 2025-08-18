8673d9f684520d5aab3c049ee0e9feac97f799d9
WongtonMein <190700700+WongtonMein@users.noreply.github.com>
2025-08-17 23:19:35 -0700

    Added buttons to play game

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ TOP - Project Rock, Paper, Scissors
 
 Objectives:
  - Use HTML and JavaScript to build a rock, paper, scissors game that is played in the console
+ - Add button interactivity
  - Have fun?
--- javascript.js
+++ javascript.js
@@ -1,106 +1,134 @@
-console.log("Hello World")
+// console.log("Hello World")
 
 // 0 = "rock"
 // 1 = "paper"
 // 2 = "scissors"
 
-// getComputerChoice function
 function getComputerChoice() {
-
-    // use Math.random() to determine computerChoice
     let computerInt = Math.floor(Math.random() * 3)
-
-    // test with console.log()
-    // console.log(computerInt)
-
-    // return computerChoice as number value
     return computerInt
 };
 
-// getHumanChoice function
 function getHumanChoice() {
+    let returnValue
+    const rockButton = document.getElementById("rock");
+    const paperButton = document.getElementById("paper");
+    const scissorsButton = document.getElementById("scissors");
 
-    // prompt human for rock, paper, or scissors
-    let humanChoice = prompt("Rock, Paper, or Scissors?")
+    rockButton.addEventListener("click", () => {
+        returnValue = 0
+    }); 
+    paperButton.addEventListener("click", () => {
+        returnValue = 1
+    });
+    scissorsButton.addEventListener("click", () => {
+        returnValue = 2
+    });
 
-    // prompt changed toLowerCase() to remove case sensitivity
-    humanChoice = humanChoice.toLowerCase()
-
-    // test with console.log()
-    // console.log(humanChoice)
-
-    // return humanChoice as number value
-    if (humanChoice == "rock")
-        return 0
-    else if (humanChoice == "paper")
-        return 1
-    else if (humanChoice == "scissors")
-        return 2
+    return returnValue;
 };
     
-// helper function to convert number value to word for playRound() console.log output
 function convertNumberToWord(choice) {
-    if (choice == 0)
+    if (choice === 0)
         return "Rock"
-    else if (choice == 1)
+    else if (choice === 1)
         return "Paper"
-    else if (choice == 2)
+    else if (choice === 2)
         return "Scissors"
 }
 
-// playRound function - parameters getComputerChoice, getHumanChoice
 function playRound(computerChoice, humanChoice) {
 
     let computerChoiceWord = convertNumberToWord(computerChoice)
     let humanChoiceWord = convertNumberToWord(humanChoice)
 
-    console.log("Player choice: " + humanChoiceWord)
-    console.log("Computer choice: " + computerChoiceWord)
+    console.log(`Player Choice: ${humanChoiceWord}`)
+    console.log(`Computer Choice: ${computerChoiceWord}`)
 
-    // console.log() results
-    if (humanChoice == computerChoice) {
+    if (humanChoice === computerChoice) {
         console.log("Tie!");
-    } else if (humanChoice == computerChoice + 1 || humanChoice == computerChoice - 2) {
-        console.log("You win! " + humanChoiceWord + " beats " + computerChoiceWord)
-        humanScore++ // increment humanScore variable
+    } else if (humanChoice === computerChoice + 1 || humanChoice === computerChoice - 2) {
+        console.log(`You win! ${humanChoiceWord} beats ${computerChoiceWord}`)
+        scores.humanScore++
     } else {
-        console.log("You lose! " + computerChoiceWord + " beats " + humanChoiceWord)
-        computerScore++ // increment computerScore variable
+        console.log(`You lose! ${computerChoiceWord} beats ${humanChoiceWord}`)
+        scores.computerScore++
     }
 };
 
-// create computerScore variable
-let computerScore = 0;
+function displayScore(computerScore, humanScore) {
+    console.log(`Player Score: ${humanScore} | Computer Score: ${computerScore}`)
+};
 
-// create humanScore variable
-let humanScore = 0;
+function scoresFunc() {
+    const scores = {
+        computerScore: 0,
+        humanScore: 0
+    }
+    return scores
+};
 
-// playGame function
 function playGame() {
+    scores = scoresFunc()
     let i = 1
 
     // sets number of rounds
-    let gameRounds = 5
+    let maxScore = 5
 
     // calls playRound()
-    while (i <= gameRounds) {
-        
+    if (scores.computerScore < maxScore && scores.humanScore < maxScore) {
         // prompt for new humanSelection
-        const computerSelection = getComputerChoice()
-        const humanSelection = getHumanChoice()
-
+        // const computerSelection = getComputerChoice()
+        // const humanSelection = getHumanChoice()
+        
         console.log("Round " + i)
+
         // play round
-        playRound(computerSelection, humanSelection)
+
+        const rockButton = document.getElementById("rock");
+        const paperButton = document.getElementById("paper");
+        const scissorsButton = document.getElementById("scissors");
+
+        let computerSelection = getComputerChoice()
+        // let humanSelection = getHumanChoice()
+
+        const bttn = document.querySelector(".game-buttons");
+
+        bttn.addEventListener("click", () => {
+            return computerSelection = getComputerChoice()
+        });
+        // bttn.addEventListener("click", () => {
+        //     playRound(computerSelection, humanSelection);
+        // });
+
+        rockButton.addEventListener("click", () => {
+            playRound(computerSelection, 0)});
+        paperButton.addEventListener("click", () => {
+            playRound(computerSelection, 1)});
+        scissorsButton.addEventListener("click", () => {
+            playRound(computerSelection, 2)});
 
         // console.log() current scores
-        console.log("Player score: " + humanScore)
-        console.log("Computer score: " + computerScore)
+        bttn.addEventListener("click", () => {
+            displayScore(scores.computerScore, scores.humanScore);
+        });
+
+
+
+        // const btn = document.querySelector("button");
+
+        // btn.addEventListener("click", playRound(computerSelection, humanSelection));
+        // playRound(computerSelection, humanSelection)
 
         // increments game round counter
         i++;
+
+
     }
 };
-    
+
+let humanChoice = getHumanChoice()
+console.log(humanChoice)
+
 // calls playGame()
-playGame()
+// playGame()
